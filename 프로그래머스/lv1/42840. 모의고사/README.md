0165b396347a5fda45bff8a46de8cf20302c9444
yurim lee <77326325+urimL@users.noreply.github.com>
2023-09-16 15:08:02 +0900

    [level 1] Title: 모의고사, Time: 3.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#qna) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 3.24 ms
+메모리: 10.3 MB, 시간: 3.67 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,6 +1,6 @@
 def solution(answers):
     answer = []
-    score1,score2,score3 = 0,0,0
+    score=[0,0,0]
     
     how1 = [1, 2, 3, 4, 5]
     how2 = [2, 1, 2, 3, 2, 4, 2, 5]
@@ -8,19 +8,16 @@ def solution(answers):
     
     for i in range(len(answers)):
         if answers[i]==how1[i%len(how1)]:
-            score1+=1
+            score[0]+=1
         if answers[i]==how2[i%len(how2)]:
-            score2+=1
+            score[1]+=1
         if answers[i]==how3[i%len(how3)]:
-            score3+=1
+            score[2]+=1
             
-    max_score = max(score1,score2,score3)
-            
-    if max_score == score1:
-        answer.append(1)
-    if max_score == score2:
-        answer.append(2)
-    if max_score == score3:
-        answer.append(3)
-           
+    max_score = max(score)
+    
+    for i in range(len(score)):
+        if max_score==score[i]:
+            answer.append(i+1)
+                   
     return answer
