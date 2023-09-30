8c1cba1f2bab5fd096501ab1c6e24a5335ddb51f
yurim lee <77326325+urimL@users.noreply.github.com>
2023-09-30 14:44:31 +0900

    [unrated] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -5,26 +5,11 @@ def solution(babbling):
     tmp = []
     
     for i in babbling:
-        i=i.replace("aya","1")
-        i=i.replace("ye","2")
-        i=i.replace("woo","3")
-        i=i.replace("ma","4")
-        
-        for j in range(len(i)-1):
-            if i[j]==i[j+1] or (i[j]<"1" or i[j]>"4"):
-                ch = False
-                print("E")
-                
-        if i[-1]<"1" or i[-1]>"4":
-            ch=False
-            print("Y")
-
-        print(i)
-        print(ch)
-        if ch:
+        for j in ['aya','ye','woo','ma']:
+            if j*2 not in i:
+                i=i.replace(j,'#')
+        if len(i)==i.count('#'):
             answer+=1
-        
-        ch=True
 
 
     return answer
