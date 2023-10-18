99538c4df4ccbd6f89a85a19d56cd0577fbe8d7c
yurim lee <77326325+urimL@users.noreply.github.com>
2023-10-18 15:12:37 +0900

    [Silver IV] Title: 카드2, Time: 224 ms, Memory: 50924 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+n = int(input())
+q = deque()
+
+for i in range(1,n+1):
+    q.append(i)
+
+while len(q)>1:
+    if q:
+        q.popleft()
+    if q:
+        q.append(q.popleft())
+
+print(q[0])
