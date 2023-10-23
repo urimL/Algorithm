a0b1ed4a2c9d74565e071b245d3320a73db42694
yurim lee <77326325+urimL@users.noreply.github.com>
2023-10-23 16:51:33 +0900

    [Bronze II] Title: Strfry, Time: 192 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -5,27 +5,21 @@ n = int(input())
 
 for i in range(n):
     a,b = map(str,input().split())
-    a,b = list(a),list(b)
-    visited = [False]*len(a)
-    check = False
+    a = "".join(sorted(a))
+    b = "".join(sorted(b))
 
     if len(a)!=len(b):
         print('Impossible')
         continue
 
-    for j in range(len(a)):
-        if a[j] not in b:
-            print('Impossible')
+    for i in range(len(a)):
+        if a[i]!=b[i]:
             check = False
             break
-        elif a[j] in b and not visited[j]:
-            visited[j]=True
-            b.remove(a[j])
-            check = True
         else:
-            print('Impossible')
-            check = False
-            break
+            check = True
 
     if check:
         print('Possible')
+    else:
+        print('Impossible')
