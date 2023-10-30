ccaa81760e0250e6643aebf077692673c2dab8e9
yurim lee <77326325+urimL@users.noreply.github.com>
2023-10-30 17:25:08 +0900

    [Silver II] Title: N과 M (12), Time: 72 ms, Memory: 33164 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,29 @@
+import sys
+from itertools import product
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+visited = [False]*n
+
+tmp = []
+answer = []
+
+def dfs():
+    if len(tmp)==m:
+            answer.append(tmp[:])
+            return
+        
+    for i in range(len(arr)):
+        if not tmp or tmp[-1]<=arr[i]:
+            tmp.append(arr[i])
+            dfs()
+            tmp.pop()
+
+dfs()
+answer = sorted(list(set(map(tuple,answer))))
+
+for i in answer:
+    for j in i:
+        print(j,end=' ')
+    print()
