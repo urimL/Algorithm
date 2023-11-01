e64a1c3273c1e715bd5c16a91e74292f9626180c
yurim lee <77326325+urimL@users.noreply.github.com>
2023-11-01 15:15:31 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 100 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+n,k = map(int,input().split())
+check = [True]*(n+1)
+answer=0
+
+for i in range(2,n+1):
+  
+    for j in range(i,n+1):
+        if j%i==0 and check[j]:
+            check[j]=False
+            answer+=1
+
+
+            if answer==k:
+                print(j)
+                break
