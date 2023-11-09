4d7310139510cae1fb17d18c34ff431b0d48929c
yurim lee <77326325+urimL@users.noreply.github.com>
2023-11-09 13:18:46 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,17 +1,5 @@
+
+2
+3
 def solution(emergency):
-    answer = []
-    tmp = []
-
-    for i in emergency:
-        tmp.append([i,emergency.index(i)+1])
-    
-    tmp.sort(reverse=True)
-    
-    for i in range(len(tmp)):
-        tmp[i].append(i+1)
-    
-    tmp.sort(key=lambda x:x[1])
-    for i in tmp:
-        answer.append(i[2])
-
-    return answer
+    return [sorted(emergency, reverse=True).index(e) + 1 for e in emergency]
