85a27d6e811c0057f6d93bc9da10b132aecd961f
yurim lee <77326325+urimL@users.noreply.github.com>
2023-11-17 13:23:52 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -2,19 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-answer = 0
 
 count = [[0]*2 for _ in range(n+2)]
 count[1][0] = 1
 count[2][1] = 1
 
-if 1<=n<=2:
-    answer = 1
+for i in range(3,n+1):
+    count[i][0] = count[i-1][1]
+    count[i][1] = count[i-1][0]+count[i-1][1]
 
-else:
-    for i in range(3,n+1):
-        count[i][0] = count[i-1][1]
-        count[i][1] = count[i-1][0]+count[i-1][1]
-        answer = count[i][0]+count[i][1]
-
-print(answer)
+print(count[n][0]+count[n][1])
