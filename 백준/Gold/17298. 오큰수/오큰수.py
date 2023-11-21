24c504dee6aa597eb8cc39cae09580b589f0fd84
yurim lee <77326325+urimL@users.noreply.github.com>
2023-11-21 13:09:03 +0900

    [Gold IV] Title: 오큰수, Time: 1024 ms, Memory: 155580 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,14 +1,26 @@
-import sys
-input = sys.stdin.readline
+def solution():
+    n = int(input())
+    nums = list(map(int,input().split()))
+    answer = [0]*n
+    st = []
 
-n = int(input())
-num = list(map(int,input().split()))
-answer,st = [-1]*n,[]
+    for i in range(n-1,-1,-1):
+        if not st:
+            answer[i] = -1
+            st.append(nums[i])
+        else:
+            while st:
+                if nums[i] < st[-1]:
+                    answer[i] = st[-1]
+                    st.append(nums[i])
+                    break
+                st.pop()
+            if not st:
+                answer[i] = -1
+                st.append(nums[i])
 
-for i in range(len(num)):
-    while st and num[st[-1]]<num[i]:
-        answer[st.pop()] = num[i]
-    st.append(i)
+    for i in answer:
+        print(i,end=' ')
 
-print(*answer)
-
+if __name__ == '__main__':
+    solution()
