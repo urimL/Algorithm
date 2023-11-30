92d68a5e591b13eb2831f42b2147fa3b93dfb94e
yurim lee <77326325+urimL@users.noreply.github.com>
2023-11-30 16:14:29 +0900

    [Silver V] Title: 날짜 계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+e,s,m = map(int,input().split())
+answer = 0
+year = 1
+
+while True:
+    if (year-e)%15==0 and (year-s)%28==0 and (year-m)%19==0:
+        break
+    year+=1
+    
+print(year)
