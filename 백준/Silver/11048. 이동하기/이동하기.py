1514cf0d7823852d6779aa5dedefdac0ab2ffc2c
yurim lee <77326325+urimL@users.noreply.github.com>
2023-12-04 14:40:47 +0900

    [Silver II] Title: 이동하기, Time: 1024 ms, Memory: 78612 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+candy = []
+dp = [[0]*(m+1) for _ in range(n+1)]
+
+for i in range(n):
+    candy.append(list(map(int,input().split())))
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        dp[i][j] = candy[i-1][j-1] + max(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+
+print(dp[n][m])
+
