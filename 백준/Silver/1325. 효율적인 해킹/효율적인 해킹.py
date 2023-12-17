7c76c562dd3d0eed21e8c85370efa9cfccec7df0
yurim lee <77326325+urimL@users.noreply.github.com>
2023-12-17 14:56:57 +0900

    [Silver I] Title: 효율적인 해킹, Time: 11124 ms, Memory: 210764 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,33 +1,44 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-graph = [[] for _ in range(n+1)]
-answer = [0]*(n+1)
 
-def bfs(x):
+n, m = map(int, input().split())
+graph = [[] for i in range(n + 1)]
+answer = []
+
+
+def bfs(now):
     q = deque()
-    q.append(x)
-    visited[x]=True
+    q.append(now)
+    visited = [False] * (n + 1)
+    visited[now] = True
+    count = 1
 
     while q:
-        now = q.popleft()
-        for i in graph[now]:
+        x = q.popleft()
+        for i in graph[x]:
             if not visited[i]:
-                visited[i]=True
+                visited[i] = True
+                count += 1
                 q.append(i)
-                answer[i]+=1
-                
-for _ in range(m):
-    s,e = map(int,input().split())
-    graph[s].append(e)
 
-for i in range(1,n+1):
-    visited = [False]*(n+1)
-    bfs(i)
+    return count
 
-max_value = max(answer)
-for i in range(1,n+1):
-    if answer[i]==max_value:
-        print(i,end=' ')
+
+max_v = 1
+for i in range(m):
+    v, e = map(int, input().split())
+    graph[e].append(v)
+
+for i in range(1, n + 1):
+    cnt = bfs(i)
+    if cnt > max_v:
+        max_v = cnt
+        answer.clear()
+        answer.append(i)
+    elif cnt == max_v:
+        answer.append(i)
+
+print(*answer)
