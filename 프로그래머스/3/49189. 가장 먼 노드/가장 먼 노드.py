34e487162efdd10b1f0901fb0ac8eb39ae82745d
yurim lee <77326325+urimL@users.noreply.github.com>
2024-01-23 14:39:47 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.58 ms, Memory: 19.7 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -3,30 +3,24 @@ from collections import deque
 def solution(n, edge):
     answer = 0
     graph = [[] for _ in range(n+1)]
-    count = [0]*(n+1)
+    count = [-1]*(n+1)
     
     for i in edge:
         v,e = i
         graph[v].append(e)
         graph[e].append(v)
         
-    def bfs(x):
-        q = deque([x])
+    q = deque([1])
+    count[1] = 0
+    
+    while q:
+        now = q.popleft()
         
-        while q:
-            now = q.popleft()
-            if not visited[now]:
-                visited[now] = True
-                for i in graph[now]:
-                    if not visited[i]:
-                        q.append(i)
-                        if count[i]==0:
-                            count[i] = count[now]+1
-                        else:
-                            count[i] = min(count[i],count[now]+1)
-                                           
-    visited = [False]*(n+1)
-    bfs(1)
+        for i in graph[now]:
+            if count[i] == -1:
+                q.append(i)
+                count[i] = count[now]+1
+    
     answer = count.count(max(count))
         
     return answer
