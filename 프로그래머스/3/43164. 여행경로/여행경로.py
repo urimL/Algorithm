468f0583cc8e203db5a1a8105f260ba22c61b85d
yurim lee <77326325+urimL@users.noreply.github.com>
2024-01-24 15:14:15 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,21 +1,6 @@
-from collections import defaultdict
-
-def solution(ticket):
+def solution(tickets):
     answer = []
-    ticket.sort(reverse = True)
-    d = defaultdict(list)
     
-    for s,e in ticket:
-        d[s].append(e)
-
-    st = ['ICN']
-    path = []
     
-    while st:
-        now = st[-1]
-        
-        if not d[now] or len(d[now]) == 0:
-            path.append(st.pop())
-        else:
-            st.append(d[now].pop())
-    return path[::-1]
+    for t in tickets:
+    return answer
