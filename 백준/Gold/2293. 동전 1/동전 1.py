71ae8dbf4960cd824d071ffb71ad526cb1e95350
yurim lee <77326325+urimL@users.noreply.github.com>
2024-03-09 11:31:15 +0900

    [Gold V] Title: 동전 1, Time: 172 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,18 +1,15 @@
 import sys
-from collections import deque
+
 input = sys.stdin.readline
 
-n, m = map(int,input().split())
-coin = []
-dp = [0]*(m+1)
+n, m = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+dp = [0] * (m + 1)
 dp[0] = 1
 
-for i in range(n):
-    coin.append(int(input()))
-coin.sort()
-
-for c in coin:
+coins.sort()
+for c in coins:
     for i in range(c,m+1):
         dp[i] += dp[i-c]
 
-print(dp[m])
+print(dp[-1])
