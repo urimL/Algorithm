71c81618eaa7b6624a670d0fb3bfa6dd37612710
yurim lee <77326325+urimL@users.noreply.github.com>
2024-08-16 13:59:05 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,19 @@
+def solution(n, words):
+    #실패 : 다른 글자로 시작 or 같은 단어
+    #실패시 반환 : 차례와 해당 차례의 사람이 말한 순서 (i//n+1)
+    answer = [0,0]
+    tmp = [words[0]]
+    cnt = 1
+    
+    for i in range(1,len(words)):
+        cnt = i%n+1
+        
+        if (words[i] in tmp) or (words[i-1][-1] != words[i][0]):
+            answer = [cnt,i//n+1]
+            print(cnt,i,n)
+            break
+            
+        else:
+            tmp.append(words[i])
+
+    return answer
