6a2c4a82d32f4f77a7866f5c7af143699a60e564
yurim lee <77326325+urimL@users.noreply.github.com>
2024-08-28 17:56:45 +0900

    [level 3] Title: 부대복귀, Time: 643.77 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/README.md
+++ 프로그래머스/3/132266. 부대복귀/README.md
@@ -1,10 +1,10 @@
 # [level 3] 부대복귀 - 132266 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266) 
 
 ### 성능 요약
 
-메모리: 39.1 MB, 시간: 103.68 ms
+메모리: 109 MB, 시간: 643.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 12:46:42
+2024년 08월 28일 17:56:44
 
 ### 문제 설명
 
--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -3,35 +3,33 @@ from collections import deque
 def solution(n, roads, sources, destination):
     answer = []
     graph = [[] for _ in range(n+1)]
-    
-    for r in roads:
-        s,e = r
+    for s,e in roads:
         graph[s].append(e)
         graph[e].append(s)
         
+    times = [500000001]*(n+1)
+    times[destination] = 0
+        
     def bfs(x):
-        q = deque([x])
-        visited = [False] * (n+1)
+        q = deque()
+        q.append((x,0))
+        visited = [False]*(n+1)
         visited[x] = True
         
         while q:
-            now = q.popleft()
-            
+            now, cnt = q.popleft()
+
             for i in graph[now]:
                 if not visited[i]:
-                    cost[i] = min(cost[i], cost[now] + 1)   
-                    q.append(i)
-                    visited[i] = True
-        
+                    if cnt+1 <= times[i]:
+                        times[i] = cnt + 1
+                        visited[i] = True
+                        q.append((i,cnt+1))
     
-    cost = [500001] * (n+1)
-    cost[destination] = 0
     bfs(destination)
-    
     for s in sources:
-        if cost[s] == 500001:
+        if times[s] == 500000001:
             answer.append(-1)
         else:
-            answer.append(cost[s])
-    
+            answer.append(times[s])
     return answer
