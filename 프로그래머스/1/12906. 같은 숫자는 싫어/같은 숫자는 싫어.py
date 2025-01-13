496dc4f83210479d6440382fe2565b609ca4c6da
yurim lee <77326325+urimL@users.noreply.github.com>
2025-01-13 14:10:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.38 ms, Memory: 27.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+    now = -1
+    
+    for i in arr:
+        if now != i:
+            answer.append(i)
+            now = i
+        else:
+            continue
+    
+    
+    return answer
