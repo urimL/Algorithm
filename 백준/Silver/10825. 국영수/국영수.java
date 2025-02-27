455db948f1984008015dd612a5bb6f31b8ede06d
yurim lee <77326325+urimL@users.noreply.github.com>
2025-02-27 17:14:29 +0900

    [Silver IV] Title: 국영수, Time: 644 ms, Memory: 61344 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,47 +1,51 @@
 import java.util.*;
 import java.io.*;
 
+class Student {
+    String name;
+    int kor, eng, math;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	int n = Integer.parseInt(br.readLine());
-    	HashMap<String, int[]> map = new HashMap<>();
-    	List<Map.Entry<String, int[]>> list;
-    	
-    	StringTokenizer st;
-    	for (int i = 0;i<n;i++) {
-    		st = new StringTokenizer(br.readLine());
-    		String name = st.nextToken();
-    		
-    		int[] score = new int[3];
-    		score[0] = Integer.parseInt(st.nextToken());
-    		score[1] = Integer.parseInt(st.nextToken());
-    		score[2] = Integer.parseInt(st.nextToken());
-    		
-    		map.put(name, score);
-    		
-    	}
-    	
-    	list = new ArrayList<>(map.entrySet());
-		list.sort((e1, e2) -> {
-			if (e1.getValue()[0] == e2.getValue()[0]) {
-				if (e1.getValue()[1] == e2.getValue()[1]) {
-					if(e1.getValue()[2] == e2.getValue()[2]) {
-						return e1.getKey().compareTo(e2.getKey());
-					}
-					return e2.getValue()[2] - e1.getValue()[2];
-				} return e1.getValue()[1] - e2.getValue()[1];
-			} return e2.getValue()[0]  - e1.getValue()[0];
-		});
-    	
-    	StringBuilder sb = new StringBuilder();
-    	
-    	for(Map.Entry<String, int[]> e: list) {
-    		sb.append(e.getKey()).append("\n");
-    	
-    	}
-    	
-    	System.out.println(sb);
+    // 생성자
+    Student(String name, int kor, int eng, int math) {
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Student> students = new ArrayList<>();
+        StringTokenizer st;
+
+        // 입력 받기
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int kor = Integer.parseInt(st.nextToken());
+            int eng = Integer.parseInt(st.nextToken());
+            int math = Integer.parseInt(st.nextToken());
+
+            students.add(new Student(name, kor, eng, math));
+        }
+
+        // 정렬 수행
+        students.sort((s1, s2) -> {
+            if (s1.kor != s2.kor) return s2.kor - s1.kor;  // 국어 내림차순
+            if (s1.eng != s2.eng) return s1.eng - s2.eng;  // 영어 오름차순
+            if (s1.math != s2.math) return s2.math - s1.math;  // 수학 내림차순
+            return s1.name.compareTo(s2.name);  // 이름 오름차순 (사전순)
+        });
+
+        // 출력 최적화
+        StringBuilder sb = new StringBuilder();
+        for (Student s : students) {
+            sb.append(s.name).append("\n");
+        }
+        System.out.println(sb);
     }
 }
