834f83007f5631ad672c09d564fe65d115039a7d
yurim lee <77326325+urimL@users.noreply.github.com>
2025-07-09 13:00:28 +0900

    [level 2] Title: 더 맵게, Time: 1584.97 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 64 MB, 시간: 1617.90 ms
+메모리: 50.8 MB, 시간: 1584.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 17일 14:00:36
+2025년 07월 09일 13:00:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,22 +1,15 @@
 import heapq
+
 def solution(scoville, k):
     answer = 0
-    hq = []
+    heapq.heapify(scoville)
     
-    for s in scoville:
-        heapq.heappush(hq,s)
-        
-    while True:
-        if hq[0] < k:
-            now = heapq.heappop(hq)
-
-            if hq:
-                new = now + heapq.heappop(hq)*2
-            else:
-                return -1
-            
-            heapq.heappush(hq, new)
-            answer += 1
-            
-        else:
+    while scoville:
+        first = heapq.heappop(scoville)
+        if first >= k:
             return answer
+        if not scoville:
+            return -1
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville, first + second * 2)
+        answer += 1
