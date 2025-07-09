cb0ec9e919b3fcfde881739a56393a8a2a0cd3b0
yurim lee <77326325+urimL@users.noreply.github.com>
2025-07-09 12:32:14 +0900

    [level 2] Title: 주식가격, Time: 45.86 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 75.37 ms
+메모리: 18.9 MB, 시간: 45.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 27일 15:30:20
+2025년 07월 09일 12:32:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,21 @@
 from collections import deque
 def solution(prices):
-    time = []
-    q = deque(prices)
-    
-    while q:
-        price = q.popleft()
-        t = 0
-        for i in q:
-            t += 1
-            if price > i:
-                break
-        time.append(t)        
+    answer = [0] * len(prices)
+    st = []
+    for i in range(len(prices)):
+        if not st or st[-1][0] <= prices[i]:
+            st.append([prices[i], i])
+        else:
+            while st and st[-1][0] > prices[i]:
+                now = st.pop()
+                answer[now[1]] += i - now[1]
+            st.append([prices[i],i])
         
-    return time
+    cnt = 0
+    print(st)
+    while st:
+        total = len(prices) - 1
+        now = st.pop()
+        answer[now[1]] += total - now[1]
+        
+    return answer
