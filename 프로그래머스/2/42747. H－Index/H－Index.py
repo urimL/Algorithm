587960917be42d6770baa6bb451528718b890a63
yurim lee <77326325+urimL@users.noreply.github.com>
2025-08-21 11:33:57 +0900

    [level 2] Title: H-Index, Time: 0.18 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,16 +1,8 @@
 def solution(citations):
     answer = 0
-    total = len(citations)
+    citations.sort(reverse = True)
     
-    citations.sort()
-
-    for i in range(len(citations)):
-        if citations[i] > total - i:
-            answer = max(answer, total - i)
-            break
-        if citations[i] >= i: 
-            answer = citations[i]
-        
-    if answer == 0:
-        answer = min(total, citations[0])
+    for c in citations:
+        if c > answer:
+            answer += 1
     return answer
