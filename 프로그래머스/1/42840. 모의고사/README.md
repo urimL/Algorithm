487627c265c79b8f2623cb9c285ea29d6bb67aaf
yurim lee <77326325+urimL@users.noreply.github.com>
2025-08-28 11:21:00 +0900

    [level 1] Title: 모의고사, Time: 4.69 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 9.29 MB, 시간: 11.71 ms
+메모리: 9.2 MB, 시간: 4.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 05일 15:00:24
+2025년 08월 28일 11:20:57
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,16 +1,18 @@
 def solution(answers):
     answer = []
-    student = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
-    result = [0,0,0]
-    
-    for i in range(len(answers)):
-        for j in range(len(student)):
-            if answers[i] == student[j][i%len(student[j])]:
-                result[j] += 1
+    student = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
+    score = []
+    for s in student:
+        count = 0
+        for i in range(len(answers)):
+            length = len(s)
+            if answers[i] == s[i%length]:
+                count += 1
         
-    max_score = max(result)
-    for i in range(3):
-        if result[i] == max_score:
-            answer.append(i+1)
+        score.append(count)
     
+    max_score = max(score)
+    for i in range(3):
+        if max_score == score[i]:
+            answer.append(i+1)
     return answer
