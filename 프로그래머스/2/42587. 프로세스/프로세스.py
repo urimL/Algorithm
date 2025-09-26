52d72d5104fb30260ec21896b461fc45ac326ec9
yurim lee <77326325+urimL@users.noreply.github.com>
2025-09-26 15:28:35 +0900

    [level 2] Title: 프로세스, Time: 0.90 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,20 +2,17 @@ from collections import deque
 
 def solution(priorities, location):
     answer = 0
-    MAX = max(priorities)
     q = deque()
-
-    for p in range(len(priorities)):
-        q.append([priorities[p], p])
+    
+    for idx, p in enumerate(priorities):
+        q.append([idx, p])
     
     while q:
-        now = q.popleft()
-        if now[0] < MAX:
-            q.append(now)
+        idx, p = q.popleft()
+        if any(p < other[1] for other in q):
+            q.append([idx, p])
         else:
             answer += 1
-            if now[1] == location:
+            if location == idx:
                 return answer
-            MAX = max(q, key=lambda x: x[0])[0]
     
-    return answer
