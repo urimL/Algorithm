046078be5a33528d274097023af8e3b58e4b7574
yurim lee <77326325+urimL@users.noreply.github.com>
2025-12-20 11:12:27 +0900

    [level 2] Title: 주식가격, Time: 26.85 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,21 +1,16 @@
-from collections import deque
-
 def solution(prices):
     answer = [0 for _ in range(len(prices))] 
-    q = deque() 
-    new_prices = []
-    for i in range(len(prices)):
-        new_prices.append([i, prices[i]])
+    st = []
     
-    for p in new_prices:
-        while q and q[-1][1] > p[1]:
-            answer[q[-1][0]] = p[0] - q[-1][0]
-            q.pop()
-        else:
-            q.append(p)
+    for i, price in enumerate(prices):
+        while st and prices[st[-1]] > price:
+            now = st.pop()
+            answer[now] = i - now
+        st.append(i)
     
-    while q:
-        answer[q[0][0]] = len(prices) - q[0][0]-1
-        q.popleft()
-    
+    n = len(prices)
+    while st:
+        now = st.pop()
+        answer[now] = n - 1 - now
+  
     return answer
