351b57753f32f84a44c34797c9caac03716890db
yurim lee <77326325+urimL@users.noreply.github.com>
2026-01-05 22:30:23 +0900

    [level 2] Title: 롤케이크 자르기, Time: 656.20 ms, Memory: 49.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,18 +1,18 @@
 from collections import Counter
- 
+
 def solution(topping):
     answer = 0
-    chulsu = Counter(topping) 
+    #초기 세팅 : 철수가 모두 가진 상황
+    chulsu = Counter(topping)
     brother = set()
- 
+    
     for t in topping:
         chulsu[t] -= 1
         brother.add(t)
- 
+        
         if chulsu[t] == 0:
             chulsu.pop(t)
-            
         if len(chulsu) == len(brother):
             answer += 1
-            
+
     return answer
