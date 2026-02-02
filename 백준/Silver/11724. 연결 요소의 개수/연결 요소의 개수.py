c92734bf29c1c362fe893f0fb382c7b1a1ed680e
yurim lee <77326325+urimL@users.noreply.github.com>
2026-02-03 08:37:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 67020 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,34 @@
 import sys
 from collections import deque
-sys.setrecursionlimit(10000)
+
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-visited = [False]*(n+1)
-
-def dfs(x):
-    visited[x] = True
-    for i in graph[x]:
-        if not visited[i]:
-            dfs(i)
-
-answer = 0
+n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-for i in range(m):
-    s,e = map(int,input().split())
+visited = [False] * (n+1)
+answer = 0
+
+for _ in range(m):
+    s, e = map(int, input().split())
     graph[s].append(e)
     graph[e].append(s)
+    
 
-for i in range(1,n+1):
+def bfs(t):
+    q = deque([t])
+    visited[t] = True
+
+    while q:
+        now = q.popleft()
+        
+        for x in graph[now]:
+            if not visited[x]:
+                q.append(x)
+                visited[x] = True
+
+for i in range(1, n+1):
     if not visited[i]:
-        dfs(i)
-        answer+=1
+        bfs(i) 
+        answer += 1
 
 print(answer)
