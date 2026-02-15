0d2d797129880410f0fc349cf51f0f798fbbee62
yurim lee <77326325+urimL@users.noreply.github.com>
2026-02-15 12:14:11 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,23 +1,18 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int idx = 0;
+        int i = 0;
         
-        for (int[] arr: commands) {
-            int start = arr[0]-1; int end = arr[1]-1; int cnt = arr[2]-1;
-            int[] tmp = new int[end-start+1];
-            
-            for (int i=start;i<=end;i++){
-                tmp[i-start] = array[i];
-            }
-            
+        for (int[] c: commands) {
+            int start = c[0]-1; int end = c[1]; int idx = c[2]-1;
+            int[] tmp = Arrays.copyOfRange(array, start, end);
             Arrays.sort(tmp);
-            answer[idx++] = tmp[cnt];
-            
-        }    
+            answer[i++] = tmp[idx];
+        }
+        
+        
         return answer;
     }
 }
