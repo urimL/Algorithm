931eda5e948e62f119ebb76d5f501395db2ca05a
yurim lee <77326325+urimL@users.noreply.github.com>
2026-02-15 11:51:40 +0900

    [level 2] Title: 올바른 괄호, Time: 20.12 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
-메모리: 9.95 MB, 시간: 14.36 ms
+메모리: 56.2 MB, 시간: 20.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 16:59:42
+2026년 02월 15일 11:51:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,28 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> st = new Stack<>();
-        for (int i=0;i<s.length();i++) {
-            if (s.charAt(i) == '(') {
-                st.push('(');
-            }
-            else {
-                if (st.isEmpty()) {
-                    return false;
-                }
-                st.pop();
-            }
+        boolean answer = true;
+        int count = 0;
+        
+        Deque<Character> q = new ArrayDeque<>();
+        for (int i=0;i<s.length();i++)
+            q.push(s.charAt(i));
+
+        char now = q.pop();
+        if (now=='(') return false;
+        else count++;
+        
+        while (!q.isEmpty()) {
+            if (q.peek()=='(') {
+                if (count > 0) {
+                    count--;
+                } else return false;
+            } else count++;
+            
+            q.pop();
         }
         
-        return st.isEmpty();
+        if (count != 0) return false;
+        return answer;
     }
 }
