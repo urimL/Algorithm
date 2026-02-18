b627ad36daa8ae49ef8bded8d7127c578c3ac074
yurim lee <77326325+urimL@users.noreply.github.com>
2026-02-18 10:32:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.77 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 11.52 ms
+메모리: 55.5 MB, 시간: 10.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 04일 16:20:48
+2026년 02월 18일 10:32:32
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,41 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = {-1,0,1,0};
-    int[] dy = {0,-1,0,1};
+    static int n;
+    static int m;
+    static boolean[][] visited;
     
     public int solution(int[][] maps) {
+        n = maps.length;
+        m = maps[0].length;
+        visited = new boolean[n][m];
+        int answer = bfs(new int[]{0,0}, maps);            
         
-        int answer = 0;
-        int[][] visited = new int[maps.length][maps[0].length];
-        bfs(0,0,maps,visited);
-        answer = visited[maps.length - 1][maps[0].length - 1];
-        
-        if (answer == 0) {
-           answer = -1;
-        }
         return answer;
     }
+    
+    static int bfs(int[] now,int[][] maps) {
+        Queue<int[]> q = new ArrayDeque<>();
+        int[] dx = {1,-1,0,0}; int[] dy = {0,0,1,-1};
+        q.add(now);
+        visited[0][0] = true;
         
-    public void bfs(int a,int b, int[][] maps,int[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-
-        q.add(new int[]{0,0});
-        visited[a][b] = 1;
         while (!q.isEmpty()) {
-            int[] now = q.poll();
-            int x = now[0];
-            int y = now[1];
-
+            int[] tmp = q.poll();
+            int x = tmp[0]; int y = tmp[1];
+            
             for (int i=0;i<4;i++) {
                 int nx = dx[i] + x;
                 int ny = dy[i] + y;
-
-                if (nx < 0 || nx > maps.length-1 || ny < 0 || ny > maps[0].length-1) {
-                continue;
-                }
-
-                if (visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    visited[nx][ny] = visited[x][y]+1;
+                
+                if (0<=nx && nx<n && 0<=ny && ny<m && maps[nx][ny]==1 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    maps[nx][ny] = maps[x][y] + 1;
                     q.add(new int[]{nx,ny});
                 }
-
             }
         }
+        
+        return visited[n-1][m-1] ? maps[n-1][m-1] : -1;
     }
 }
